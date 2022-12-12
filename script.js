566e7ed2576974d7a053085dffa56614977210e9
Alexis RIGOULOT <alexis.rigoulot@2bsystem.fr>
2022-12-12 14:22:57 +0100

    full animation

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+if (this.name!='fullscreen'){
+    window.open(location.href,'fullscreen','fullscreen,scrollbars')
+    }
+
 var largeurFenetre = window.innerWidth;
-console.log(largeurFenetre);
 
 if (window.innerWidth < 801) {
     var mobileInterdit = document.getElementById('mobileInterdit');
@@ -32,100 +35,132 @@ if (window.innerWidth >= 801) {
             rigolade.style.display="none";
         }
     })
+    
+    var compteurRideauxDebut = 0;
 
 // Apparation du texte d'accueil
-    setTimeout(apparitionTexte, 2000);
+    time1 = 2000;
+    compteurRideauxDebut = compteurRideauxDebut + time1;
+    setTimeout(apparitionTexte, time1);
     function apparitionTexte(){        
             var d = document.getElementById('accueil0');
             d.classList.add("vignetsApparition"); 
     }
-    setTimeout(dispartionTexte, 7000);
+    time2 = 7000;
+    compteurRideauxDebut = compteurRideauxDebut + time2;
+    setTimeout(dispartionTexte, time2);
     function dispartionTexte(){        
             var d = document.getElementById('accueil0');
             d.classList.add("vignetsDisparition"); 
     }
-    setTimeout(apparitionTexte1, 10000);
+    time3 = 10000;
+    compteurRideauxDebut = compteurRideauxDebut + time3;
+    setTimeout(apparitionTexte1, time3);
     function apparitionTexte1(){        
             var d = document.getElementById('accueil1');
             d.classList.add("vignetsApparition"); 
     }
-    setTimeout(dispartionTexte1, 14000);
+    time4 = 14000;
+    compteurRideauxDebut = compteurRideauxDebut + time4;
+    setTimeout(dispartionTexte1, time4);
     function dispartionTexte1(){        
             var d = document.getElementById('accueil1');
             d.classList.add("vignetsDisparition"); 
     }
-    // Apparation scrollbar => 15000
-    setTimeout(scrollActive, 1);
-    function scrollActive(){  
-            var a = document.getElementById('demo');
-            a.className += ' show';       
-            document.getElementsByTagName('body')[0].style.overflowY = "scroll";
+    // Apparation scrollbar & bouton scrool=> 15000
+    // setTimeout(scrollActive, 1);
+    // function scrollActive(){  
+    //         var a = document.getElementById('demo');
+    //         a.className += ' show';       
+    //         document.getElementsByTagName('body')[0].style.overflowY = "scroll";
+    // }
+
+
+    // Scroll Annonce
+    setTimeout(defilementAnnonce, 17000);
+    function defilementAnnonce(){
+        $('html, body').animate({
+            scrollTop: $("#annonce").offset().top
+        }, 5000);
+        const txtAnim2 = document.getElementById('annonce_texte1');
+        new Typewriter(txtAnim2, {
+            loop: false,
+            cursor: '',            
+        })
+        .pauseFor (5000)
+        .changeDelay (100)
+        .typeString('vous invite à leur mariage le')
+        .start ()
+
+        const txtAnim3 = document.getElementById('annonce_texte2');
+        new Typewriter(txtAnim3, {
+            loop: false,
+            cursor: '',            
+        })
+        .pauseFor (9000)
+        .changeDelay (100)
+        .typeString('10 Juin 2023')
+        .start ()
+
+        const txtAnim4 = document.getElementById('annonce_texte3');
+        new Typewriter(txtAnim4, {
+            loop: false,
+            cursor: '',            
+        })
+        .pauseFor (11000)
+        .changeDelay (30)
+        .typeString('à 16 heures à la mairie de Varambon <br> ')
+        .pauseFor (2000)
+        .typeString ('à 17 heures cérémonie laïque à la salle de Druillat <br>')
+        .pauseFor (2000)
+        .typeString ('à l\'issue de cette cérémonie, nous aurons le plaisir de vous retrouver pour l\'apéro.... ')
+        .start ()
     }
+    
+        // Scroll Programme
+        setTimeout(defilementProgramme, 45000);
+        function defilementProgramme(){
+            $('html, body').animate({
+                scrollTop: $("#programme").offset().top
+            }, 5000);
 
- // Fonction scroll auto
-    //  var scroll = document.getElementById('demo');
-    //  scroll.addEventListener("click", function(){
-    //     console.log("J\'ai clické");
-    // });
- 
+             setTimeout(apparitionPhotosProgramme, 10000);
+             function apparitionPhotosProgramme() {
+                console.log("Je passe LA");
+                var photoOpacity = document.getElementsByClassName("photos");
+    
+                for (let i = 0; i < photoOpacity.length; i++) {
+                    console.log("Je passe LA LA LA");
+                    photoOpacity[i].classList.add("modif-opacityTemps");                
+                    photoOpacity[i].classList.add("modif-opacity");                
+                }
+             }      
+        }
 
 
+        // Scroll Gallery
+        setTimeout(defilementGallery, 65000);
+        function defilementGallery(){
+            $('html, body').animate({
+                scrollTop: $("#gallery").offset().top
+            }, 5000);
+        }
 
-
-// Détection écran
-    var isInViewport = function (elem) {
-    var distance = elem.getBoundingClientRect();
-    return (
-        distance.top >= 0 &&
-        distance.left >= 0 &&
-        distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        distance.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-    };
-
-    var findMe = document.querySelector('#annonce');
-    var one = 0; 
-
-    window.addEventListener('scroll', function (event) {        
-    if (one == 0 ) { 
-        if (isInViewport(findMe)) {	
-            one = 1;
-            const txtAnim2 = document.getElementById('annonce_texte1');
-            new Typewriter(txtAnim2, {
-                loop: false,
-                cursor: '',            
-            })
-            .pauseFor (0)
-            .changeDelay (100)
-            .typeString('vous invite à leur mariage le')
-            .start ()
-
-            const txtAnim3 = document.getElementById('annonce_texte2');
-            new Typewriter(txtAnim3, {
-                loop: false,
-                cursor: '',            
-            })
-            .pauseFor (4000)
-            .changeDelay (100)
-            .typeString('10 Juin 2023')
-            .start ()
-
-            const txtAnim4 = document.getElementById('annonce_texte3');
-            new Typewriter(txtAnim4, {
-                loop: false,
-                cursor: '',            
-            })
-            .pauseFor (6000)
-            .changeDelay (30)
-            .typeString('à 16 heures à la mairie de Varambon <br> ')
-            .pauseFor (2000)
-            .typeString ('à 17 heures cérémonie laïque à la salle de Druillat <br>')
-            .pauseFor (2000)
-            .typeString ('à l\'issue de cette cérémonie, nous aurons le plaisir de vous retrouver pour l\'apéro.... ')
-            .start ()
-        } 
-    }
-    }, false);
+         // Scroll Gallery Fin
+        setTimeout(defilementFinGallery, 65000);
+        function defilementFinGallery(){
+            $('html, body').animate({
+                scrollTop: $("#finGallery").offset().top
+            }, 40000);
+        }
+         // Scroll Rideaux Fin
+        setTimeout(defilementRideauxFin, 105000);
+        function defilementRideauxFin(){
+            $('html, body').animate({
+                scrollTop: $("#rideauxFin").offset().top
+            }, 5000);
+        }
+        
 
 
 // Rideaux Fin
@@ -162,7 +197,7 @@ window.addEventListener('scroll', function(){
             // Apparation du texte 1 Fin
             setTimeout(() => {
                 if(onlyOneFin == 0) {
-                    document.getElementsByTagName('body')[0].style.overflowY = "hidden";
+                    // document.getElementsByTagName('body')[0].style.overflowY = "hidden";
                 }
                 texte1Fin.classList.add("vignetApparitionFin1"); 
                 onlyOneFin = 1;
